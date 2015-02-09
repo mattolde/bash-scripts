e587f5d0f8846b0cec259a6fd2c4d18081826797
Matt Olde <mattolde@gmail.com>
2015-02-09 12:58:14 +1100

    bash script setup

---
+++ cask_app_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+brew cask install vlc
+brew cask install vagrant
+brew cask install spotify
+brew cask install github
+brew cask install flux
+brew cask install slack
+brew cask install calibre
+brew cask install anki
+brew cask install skype
+brew cask install firefox
+brew cask install appcleaner
+brew cask install balsamiq-mockups
+brew cask install hocus-focus
+brew cask install alfred
+brew cask install bartender
---
+++ osx_defaults.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo 'Start Setting OSX Defaults'
+
+#Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 0.02
+
+#Set a shorter Delay until key repeat
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
+
+#Add a context menu item for showing the Web Inspector in web views
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+#Show the ~/Library folder
+chflags nohidden ~/Library
+
+#Store screenshots in subfolder on desktop
+mkdir ~/Desktop/Screenshots
+defaults write com.apple.screencapture location ~/Desktop/Screenshots
+
+# Set Hostname
+sudo scutil --set HostName cyber
+
+echo 'Finished Setting OSX Defaults'
